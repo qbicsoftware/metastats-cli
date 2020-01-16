7e925be7520a9fc99812e2df073f1bede50ea8de
jenniferboedker <jennifer.boedker@student.uni-tuebingen.de>
2020-01-16 16:19:04 +0100

    add test for filter use case

--- metastats-domain/src/test/groovy/life/qbic/metastats/filter/FilterExperimentDataImplSpecification.groovy
+++ metastats-domain/src/test/groovy/life/qbic/metastats/filter/FilterExperimentDataImplSpecification.groovy
@@ -3,18 +3,37 @@ package life.qbic.metastats.filter
 import spock.lang.Specification
 
 class FilterExperimentDataImplSpecification extends Specification{
+    def mockedMSMetadataPackageOutput = Mock(MSMetadataPackageOutput)
+    def filterExperimentData = new FilterExperimentDataImpl(mockedMSMetadataPackageOutput)
 
     def "incomplete metaStats-metadata-package gives a warning"(){
+        given:
+        def projectMetadata = new HashMap<String,String>()
+        //todo fill
 
+        when:
+        filterExperimentData.createMetaStatsMetadataPackage(projectMetadata)
+
+        then:
+        thrown(IllegalArgumentException)
     }
 
     def "complete metaStats-metadata-package gives no warning"(){
+        given:
+        def projectMetadata = new HashMap<String,String>()
+        //todo fill
 
+        when:
+        filterExperimentData.createMetaStatsMetadataPackage(projectMetadata)
+
+        then:
+        !thrown(IllegalArgumentException)
     }
 
-    def "metadata only contains fields described in the schema"(){
+    /** how to test that/meaningful test?
+     def "metadata only contains fields described in the schema"(){
 
     }
-
+    */
 
 }
