1d6f3b96a801af4d446a9325bcc3f7dcdba495b5
jenniferboedker <jennifer.boedker@student.uni-tuebingen.de>
2020-01-16 13:23:18 +0100

    make code testable

---
+++ metastats-domain/src/test/groovy/life/qbic/metastats/filter/FilterExperimentDataImplSpecification.groovy
@@ -0,0 +1,6 @@
+package life.qbic.metastats.filter
+
+import spock.lang.Specification
+
+class FilterExperimentDataImplSpecification extends Specification{
+}
---
+++ metastats-domain/src/test/groovy/life/qbic/metastats/request/RequestExperimentDataSpecification.groovy
@@ -0,0 +1,6 @@
+package life.qbic.metastats.request
+
+import spock.lang.Specification
+
+class RequestExperimentDataSpecification extends Specification{
+}
