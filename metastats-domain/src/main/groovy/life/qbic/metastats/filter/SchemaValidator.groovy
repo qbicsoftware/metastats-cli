5ba2a94a0245a02fdc98f50e137d9f590c593776
jnnfr <jennifer.boedker@student.uni-tuebingen.de>
2020-07-02 13:46:59 +0200

    Update metastats-domain/src/main/groovy/life/qbic/metastats/filter/SchemaValidator.groovy
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

--- metastats-domain/src/main/groovy/life/qbic/metastats/filter/SchemaValidator.groovy
+++ metastats-domain/src/main/groovy/life/qbic/metastats/filter/SchemaValidator.groovy
@@ -5,7 +5,7 @@ interface SchemaValidator {
     /**
      * Validates the metastats metadata contained in a map based on a given schema
      * @param valueMap
-     * @return
+     * @return boolean indicating the success or failure of metadata validation against the predefined schema 
      */
     boolean validateMetaStatsMetadataPackage(Map valueMap)
 
