020ae82d17d0e58d8db05493310008a1f908c335
jnnfr <jennifer.boedker@student.uni-tuebingen.de>
2020-07-02 13:49:32 +0200

    Update metastats-domain/src/main/groovy/life/qbic/metastats/filter/SequencingModeCalculator.groovy
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

--- metastats-domain/src/main/groovy/life/qbic/metastats/filter/SequencingModeCalculator.groovy
+++ metastats-domain/src/main/groovy/life/qbic/metastats/filter/SequencingModeCalculator.groovy
@@ -17,9 +17,9 @@ class SequencingModeCalculator {
         files.each { file ->
             //Filenames should look like this: SampleName_S1_L001_R1_001.fastq.gz
             //or: SampleName_S1_L001_R1.fastq.gz
+            if (!file.contains("fastq")) throw new IllegalFileType("File is not of type fastq: $file")
             if (file.contains("_R1_001.fastq") || file.contains("_R1.fastq")) R1 = true
             if (file.contains("_R2_001.fastq") || file.contains("_R2.fastq")) R2 = true
-            if (!file.contains("fastq")) throw new IllegalFileType("File is not of type fastq: $file")
         }
         //has only R1
         if (!R2) sequencingMode = SequencingMode.SINGLE_END.seqMode
