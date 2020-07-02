635042d935231bf067c0f7e687b1098dc1b6b819
jnnfr <jennifer.boedker@student.uni-tuebingen.de>
2020-07-02 13:44:03 +0200

    Update metastats-domain/src/main/groovy/life/qbic/metastats/filter/MSMetadataPackageOutput.groovy
    Co-authored-by: Steffengreiner <Steffen.Greiner@gmx.de>

--- metastats-domain/src/main/groovy/life/qbic/metastats/filter/MSMetadataPackageOutput.groovy
+++ metastats-domain/src/main/groovy/life/qbic/metastats/filter/MSMetadataPackageOutput.groovy
@@ -6,7 +6,7 @@ interface MSMetadataPackageOutput {
 
     /**
      * Creates the metasStats metadata package from the MetaStatsPackageEntries
-     * @param samples containing all validated information required a correct metadata sheet
+     * @param samples List of MetaStatsPackageEntries containing the validated sample information necessary for the creation of a correct metadata sheet
      * @return
      */
     def createMetaStatsMetadataPackage(List<MetaStatsPackageEntry> samples)
